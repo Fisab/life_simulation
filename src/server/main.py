e88a13dd9ccb3246c8009337044370d8e27b14a4
Fisab <vladsudakov5@gmail.com>
2019-01-27 23:33:34 +0300

    finish simple connection

--- src/server/main.py
+++ src/server/main.py
@@ -1,29 +1,29 @@
 import threading
-from flask import Flask
-from flask_socketio import SocketIO
-import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../client')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
 @app.route('/')
 def index():
-	print('sdf')
+	print('Someone recieve main index')
 
-	return 'Hello world!'
+	return render_template('index.html')
 
 
 @socketio.on('connect')
-def handle_message(message):
-	print('received connect: ' + message)
+def handle_connect():
+	print('Hello!')
 
 
 @socketio.on('message')
-def handle_message_(message):
-	print('received message: ' + message)
+def handle_message(data):
+	print('received message: ' + str(data))
+	emit('message', data)
 
 
 if __name__ == '__main__':
-	socketio.run(app, host='localhost', port=10001)
+	socketio.run(app, port=10001)
